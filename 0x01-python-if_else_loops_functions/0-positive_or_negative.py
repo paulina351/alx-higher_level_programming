d17c4226095db1c737075ef8093a37afed01d225
paulina udeh <udehpaulina93@gmail.com>
2023-08-08 08:06:17 -0700

    positive is better than positive nothing

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -2,3 +2,10 @@
 import random
 number = random.randint(-10, 10)
 # YOUR CODE HERE
+x = int(input("please enter an integer: "))
+if x > 0:
+    print('{} is positive')
+elif x == 0:
+    print('{} is zero')
+else:
+    print('{} is negative')
