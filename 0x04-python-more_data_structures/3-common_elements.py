63155e2871c855f7a0f2b68e2db261a72b4933b9
Paulina Udeh <udehpaulina93@gmail.com>
2023-08-17 11:57:13 -0700

    common elements

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    """
+    function that returns a set of common elements
+    ...
+
+    parameters
+    ----------
+
+    set_1 : set
+        first set of elements
+    set_2: set
+        second elements
+
+    Return:
+        the results of the operation
+
+    """
+
+    return (set_1, set_2)
