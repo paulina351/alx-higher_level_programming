c4e41c449ad57ce6e0a6216973c0aee6aef09839
Paulina Udeh <udehpaulina93@gmail.com>
2023-08-28 11:40:31 -0700

    Print and count integers m

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
+    """
+    function that prints the first x elements of a list and only integers
+    """
     count = 0
     for b in range(0, x):
         try:
