7b62c3380c5d60afcdfa88e4d70957385cc3f24b
Paulina Udeh <udehpaulina93@gmail.com>
2023-08-28 02:56:42 -0700

    Divide a list

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    Nlist = []
+    count = 0
+    for b in range(list_length):
+        try:
+            count = my_list_1[b] / my_list_2[b]
+        except(ValueError, TypeError):
+            print("Wrong type")
+            count = 0
+        except ZeroDivisionError:
+            print("division by 0")
+            count = 0
+        finally:
+            Nlist.append(count)
+    return Nlist
