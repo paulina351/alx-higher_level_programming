f08685b02d99e80c265f678d856048c3dd1c6c40
Paulina Udeh <udehpaulina93@gmail.com>
2023-08-28 12:46:09 -0700

    Divide a list

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -15,5 +15,5 @@ def list_division(my_list_1, my_list_2, list_length):
             print("out of range")
             count = 0
         finally:
-            Nlist.append(count)
+            pass
     return Nlist
