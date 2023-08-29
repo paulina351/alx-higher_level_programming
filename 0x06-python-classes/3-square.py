b30c564da53e7cddfabc5288daab871950938406
Paulina Udeh <udehpaulina93@gmail.com>
2023-08-29 08:17:07 -0700

    Area of a square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
-"""
-"""
+"""a class Square that defines a square"""
 
 
 class Square:
+    """
+    Private instance attribute: size
+    Public instance method: def area(self):
+    """
 
     def __init__(self, size=0):
+        """
+        Private instance attribute: size
+        Public instance method: def area(self):
+        """
 
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
