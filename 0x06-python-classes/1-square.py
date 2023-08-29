be7bef06c7057898c55ebeda5d9c4365f0434ff6
Paulina Udeh <udehpaulina93@gmail.com>
2023-08-29 08:12:17 -0700

    Square with size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-"""a class Square that defines a square
-
-"""
+"""a class Square that defines a square"""
 
 
 class Square:
+    """
+    Private instance attribute: size
+    Instantiation with size (no type/value verification)
+    You are not allowed to import any module
+    """
 
     def __init__(self, size):
 
