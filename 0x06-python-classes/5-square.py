9ecbd1fc8713cb78d05ba61db4af47cecfcd9e42
Paulina Udeh <udehpaulina93@gmail.com>
2023-08-29 08:30:17 -0700

    Printing a square

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -14,7 +14,7 @@ class Square:
 
     @size.setter
     def size(self, size):
-        if type(size) != int:
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
