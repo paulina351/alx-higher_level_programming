a2c4e30a6db8412ea82d6efd374225cfa19b7044
Paulina Udeh <udehpaulina93@gmail.com>
2023-09-11 08:06:33 -0700

    improve geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Improve geometry module"""
+
+
+class BaseGeometry:
+    """To improve geometry module"""
+
+    def area(self):
+        """an oop"""
+        raise Exception("area() is not implemented")
