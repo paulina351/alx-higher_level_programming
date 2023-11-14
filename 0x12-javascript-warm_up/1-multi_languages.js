af2f68931c31599dd5465e6f5c95d0613db0dc04
Paulina Udeh <udehpaulina93@gmail.com>
2023-11-13 17:27:40 -0800

    3 languages

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,2 @@
 #!/usr/bin/node
-let multiLang = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-console.log('C is fun');
-console.log('Python is cool');
-console.log('Javascript is amazing');
+console.log('C is fun\nPython is cool\nJavascript is amazing');
