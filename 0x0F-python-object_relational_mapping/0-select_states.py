103bf07bb2165a17f78cb91f837ce76d03620057
Paulina Udeh <udehpaulina93@gmail.com>
2023-11-19 22:53:29 +0000

    get all states

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""a script that lists all states from the database hbtn_0e_0_usa"""
+
+
+from sys import argv
+import MySQLdb
+
+def list_states():      
+    """Receives arguments argv to list from database
+
+    Arguments:
+        argv[1]: mysql username
+        argv[2]: mysql password
+        argv[3]: database name
+    """
+    username=argv[1]
+    password=argv[2]
+    db=argv[3]
+    connect = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=username, 
+                         passwd=password,
+                         db=db,
+                         charset="utf8"
+                         )
+
+    c = connect.cursor()
+    c.execute('SELECT * FROM states ORDER BY id ASC;')
+    query_rows = c.fetchall()
+    for row in query_rows:
+        print(row)
+    c.close()
+    connect.close()
+
+if __name__ == "__main__":
+    list_states()
