909b3471879f7294384b81c609ff5db6a9ee9976
Paulina Udeh <udehpaulina93@gmail.com>
2023-11-20 01:14:43 +0000

    cities in states

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -17,4 +17,4 @@ class City(Base):
     __tablename__ = "cities"
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey("states.id"), nullale=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
