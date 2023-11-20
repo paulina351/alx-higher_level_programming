2015a460d5d9c23d3c6dbf652e4ead3d8c97ca7d
Paulina Udeh <udehpaulina93@gmail.com>
2023-11-20 00:03:48 +0000

    all cities by state

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -10,6 +10,6 @@ if __name__ == "__main__":
     c = db.cursor()
     c.execute("SELECT * FROM `cities` as `c` \
                 INNER JOIN `states` as `s` \
-                   ON `c`.`states` as `s` \
+                   ON `c`.`state_id` = `s`.`id` \
                 ORDER BY `c`.`id`")
     print(", ".join([ct[2] for ct in c.fetchall() if ct[4] == sys.argv[4]]))
