bd54c669e4d06dea42a32d206c31960efbc53f40
Paulina Udeh <udehpaulina93@gmail.com>
2023-12-01 19:25:35 -0800

    Error code

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -2,7 +2,7 @@
 """a Python script that takes in a URL."""
 
 from urllib.request import urlopen
-from urllib.request import  Request
+from urllib.request import Request
 from sys import argv
 import urllib.error
 
@@ -11,14 +11,14 @@ def intranet_hbtn_error():
     """sends a request to the URL and displays
         the body of the response (decoded in utf-8).
     """
-    url = arg[1]
+    url = argv[1]
     req = urllib.request.Request(url)
     try:
         with urllib.request.urlopen(req) as response:
             print(response.read().decode('utf-8'))
-        except urllib.error.HTTPError as error:
+    except urllib.error.HTTPError as error:
             print("Error code: {}".format(error.code))
 
 
 if __name__ == "__main__":
-    intranet_hbtn_error():
+    intranet_hbtn_error()
